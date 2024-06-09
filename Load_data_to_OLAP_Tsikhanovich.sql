ead16060b9e03cc7ad2a53480b7f56fc83fa7652
KatTihanovich <128325686+KatTihanovich@users.noreply.github.com>
2024-06-09 23:30:14 +0300

    Update Load_data_to_OLAP_Tsikhanovich.sql

--- Load_data_to_OLAP_Tsikhanovich.sql
+++ Load_data_to_OLAP_Tsikhanovich.sql
@@ -1,17 +1,17 @@
--- Установка необходимого расширения
+-- Installing the required extension
 CREATE EXTENSION IF NOT EXISTS postgres_fdw;
 
--- Создание внешнего сервера, который подключается к 'oltp'
+-- Creating an external server that connects to ‘oltp’
 CREATE SERVER oltp_server
     FOREIGN DATA WRAPPER postgres_fdw
     OPTIONS (host 'localhost', dbname '01', port '5432');
 
--- Создание пользовательского отображения для текущего пользователя
+-- Creating a custom display for the current user
 CREATE USER MAPPING FOR CURRENT_USER
     SERVER oltp_server
     OPTIONS (user 'postgres', password '159357');
 
--- Импорт схемы из внешнего сервера
+-- Importing a schema from an external server
 IMPORT FOREIGN SCHEMA public
 FROM SERVER oltp_server
 INTO public;
@@ -21,28 +21,28 @@ DROP FUNCTION IF EXISTS transferring_data CASCADE;
 CREATE OR REPLACE FUNCTION transferring_data()
 RETURNS void AS $$
 BEGIN
-    -- Перенос данных в DimCustomer
+    -- Transferring data to DimCustomer
     INSERT INTO DimCustomer (first_name, last_name, email, phone_number, birth_date, address, postal_code)
     SELECT c.first_name, c.last_name, c.email, c.phone_number, c.birth_date, c.address, c.postal_code
     FROM public.customer c
     LEFT JOIN DimCustomer dc ON c.email = dc.email
     WHERE dc.customer_id IS NULL;
 
-    -- Перенос данных в DimSupplier
+    -- Transferring data to DimSupplier
     INSERT INTO DimSupplier (supplier_name, contact_phone, contact_email, address, city, country)
     SELECT s.supplier_name, s.contact_phone, s.contact_email, s.address, s.city, s.country
     FROM public.supplier s
     LEFT JOIN DimSupplier ds ON s.contact_email = ds.contact_email
     WHERE ds.supplier_id IS NULL;
 
-    -- Перенос данных в DimCategory
+    -- Transferring data to DimCategory
     INSERT INTO DimCategory (category_name)
     SELECT c.category_name
     FROM public.category c
     LEFT JOIN DimCategory dc ON c.category_name = dc.category_name
     WHERE dc.category_id IS NULL;
 
-    -- Перенос данных в DimProduct
+    -- Transferring data to DimProduct
     INSERT INTO DimProduct (product_name, category_id, supplier_id, price, available_quantity, description, start_date, end_date, current_flag)
     SELECT p.product_name, dc.category_id, ds.supplier_id, p.price, p.available_quantity, p.description, NOW(), '9999-12-31', TRUE
     FROM public.product p
@@ -53,7 +53,7 @@ BEGIN
     LEFT JOIN DimProduct dp ON p.product_name = dp.product_name AND dc.category_id = dp.category_id AND ds.supplier_id = dp.supplier_id
     WHERE dp.product_id IS NULL;
 
-    -- Перенос данных в DimDate
+    -- Transferring data to DimDate
     INSERT INTO DimDate (date, day, month, quarter, year)
     SELECT DISTINCT o.order_date, EXTRACT(DAY FROM o.order_date), EXTRACT(MONTH FROM o.order_date), EXTRACT(QUARTER FROM o.order_date), EXTRACT(YEAR FROM o.order_date)
     FROM public.orders o
@@ -66,7 +66,7 @@ BEGIN
     LEFT JOIN DimDate dd ON r.rental_date = dd.date
     WHERE dd.date IS NULL;
 
-    -- Перенос данных в FactSales
+    -- Transferring data to FactSales
     INSERT INTO FactSales (customer_id, product_id, date_id, quantity, total_price)
     SELECT c.customer_id, p.product_id, d.date_id, od.quantity, (od.quantity * p.price) AS total_price
     FROM public.orders o
@@ -79,7 +79,7 @@ BEGIN
     LEFT JOIN FactSales fs ON c.customer_id = fs.customer_id AND p.product_id = fs.product_id AND d.date_id = fs.date_id
     WHERE fs.sales_id IS NULL;
 
-    -- Перенос данных в FactRental
+    -- Transferring data to FactRental
     INSERT INTO FactRental (customer_id, product_id, rental_date_id, quantity, days_rented, total_price)
     SELECT c.customer_id, p.product_id, d.date_id, rd.quantity, rd.days_rented, (rd.quantity * p.price * rd.days_rented) AS total_price
     FROM public.rental r
